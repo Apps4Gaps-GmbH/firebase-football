5da67ba17164e221491e75387c46ca4a8c8aea61
Tomer Ciucran <tomercukran@gmail.com>
2016-02-03 14:21:30 +0100

    creating git repo

---
+++ FootballApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootballApp
+//
+//  Created by Tomer Ciucran on 2/3/16.
+//  Copyright © 2016 Apps4Gaps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
